be9852e285910e53f45ff744cb7fe86bf081dd3e
todoni <zoe@laon-medi.com>
2025-02-18 16:54:45 +0900

    feat: Add tests for invalid cases

--- src/test/utils/extract-svg-data.test.ts
+++ src/test/utils/extract-svg-data.test.ts
@@ -2,8 +2,26 @@ import path from "path";
 import { expect, test } from "vitest";
 import { extractSVGData } from "../../utils/utils";
 
-test("extract svg data from file", async () => {
+test("Any invalid cases should return null", async () => {
+  expect(
+    await extractSVGData(path.resolve(__dirname, "../fixture/svgs/not-exist"))
+  ).toEqual(null);
+
   expect(
     await extractSVGData(path.resolve(__dirname, "../fixture/svgs/empty"))
+  ).toEqual(null);
+
+  expect(
+    await extractSVGData(path.resolve(__dirname, "../fixture/svgs/empty.svg"))
+  ).toEqual(null);
+
+  expect(
+    await extractSVGData(
+      path.resolve(__dirname, "../fixture/svgs/none-svg.svg")
+    )
+  ).toEqual(null);
+
+  expect(
+    await extractSVGData(path.resolve(__dirname, "../fixture/svgs/invalid.svg"))
   ).toEqual(null);
 });
