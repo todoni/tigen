5318ad55b08204fc36fcf6bab83ef0059028c455
todoni <zoe@laon-medi.com>
2025-02-18 16:29:49 +0900

    feat: Add basic test

---
+++ src/test/utils/extract-svg-data.test.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { expect, test } from "vitest";
+import { extractSVGData } from "../../utils/utils";
+
+test("extract svg data from file", async () => {
+  expect(
+    await extractSVGData(path.resolve(__dirname, "../fixture/svgs/empty"))
+  ).toBe(null);
+});
