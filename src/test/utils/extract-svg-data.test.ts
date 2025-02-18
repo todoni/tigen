8aba55a53ab531b90eec5ac001ed7135bb022140
todoni <zoe@laon-medi.com>
2025-02-18 16:54:04 +0900

    feat: Use toEqual to compare only value

--- src/test/utils/extract-svg-data.test.ts
+++ src/test/utils/extract-svg-data.test.ts
@@ -5,5 +5,5 @@ import { extractSVGData } from "../../utils/utils";
 test("extract svg data from file", async () => {
   expect(
     await extractSVGData(path.resolve(__dirname, "../fixture/svgs/empty"))
-  ).toBe(null);
+  ).toEqual(null);
 });
